74ffb9629c7baa301bd0c2c0ce982199e3a55fe5
zhouzhiqiang <3223988055@qq.com>
2023-07-09 09:23:16 +0800

    食物功能

---
+++ Assets/FoodManager/FoodManager.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace test
+{
+	public enum FoodType
+	{
+		Apple = 0,
+		Banana = 1,
+		Sugarcane = 2, //甘蔗
+		Grape = 3,     //葡萄
+		Cucumber = 4,   //黄瓜
+
+		Steak = 5,     //肉排
+		Drumstick = 6, //鸡腿
+		Fish = 7,
+		BeefHead = 8,   //牛头
+		RoastedWholeLamb = 9, //烤全羊
+
+		WhiteCarnation = 10,      //白康乃馨
+		YellowChrysanthemum = 11, //黄菊花
+		RedRose = 12              //红玫瑰
+	}
+	public struct FOOD
+	{
+		public FoodType name;
+		public List<int> shapes;
+		public List<int> attr;  // 生命力，精神力，适应力
+		public List<int> level;
+	};
+
+	public class CFood
+	{
+		Dictionary<int, FOOD> map;
+		public void init()
+		{
+
+			// A #0, Fruit_A
+			FOOD Fruit_A;
+
+			Fruit_A.name = FoodType.Apple;
+
+			Fruit_A.shapes = new List<int>();
+			Fruit_A.shapes.Add(1);
+
+			Fruit_A.attr = new List<int>();
+			Fruit_A.attr.Add(1);
+			Fruit_A.attr.Add(0);
+			Fruit_A.attr.Add(0);
+
+			Fruit_A.level = new List<int>();
+			Fruit_A.level.Add(1);
+			Fruit_A.level.Add(2);
+			Fruit_A.level.Add(3);
+
+			map.Add(0, Fruit_A);
+
+			// =================================
+			// B #1, Fruit_B
+			FOOD Fruit_B;
+			Fruit_B.name = FoodType.Banana;
+
+			Fruit_B.shapes = new List<int>();
+			Fruit_B.shapes.Add(4);
+			Fruit_B.shapes.Add(5);
+			Fruit_B.shapes.Add(7);
+
+			Fruit_B.attr = new List<int>();
+			Fruit_B.attr.Add(5);
+			Fruit_B.attr.Add(-2);
+			Fruit_B.attr.Add(0);
+
+			Fruit_B.level = new List<int>();
+			Fruit_B.level.Add(1);
+			Fruit_B.level.Add(2);
+			Fruit_B.level.Add(3);
+			Fruit_B.level.Add(4);
+
+			map.Add(1, Fruit_B);
+
+			// =================================
+			// C #2, Fruit_C
+			FOOD Fruit_C;
+
+			Fruit_C.name = FoodType.Sugarcane;
+
+			Fruit_C.shapes = new List<int>();
+			Fruit_C.shapes.Add(8);
+			Fruit_C.shapes.Add(10);
+			Fruit_C.shapes.Add(14);
+
+			Fruit_C.attr = new List<int>();
+			Fruit_C.attr.Add(6);
+			Fruit_C.attr.Add(-1);
+			Fruit_C.attr.Add(-1);
+
+			Fruit_C.level = new List<int>();
+			Fruit_C.level.Add(3);
+			Fruit_C.level.Add(4);
+			Fruit_C.level.Add(5);
+
+			map.Add(2, Fruit_C);
+
+			// =================================
+			// D #3, Fruit_D
+			FOOD Fruit_D;
+
+			Fruit_D.name = FoodType.Grape;
+
+			Fruit_D.shapes = new List<int>();
+			Fruit_D.shapes.Add(9);
+			Fruit_D.shapes.Add(11);
+			Fruit_D.shapes.Add(13);
+			Fruit_D.shapes.Add(15);
+			Fruit_D.shapes.Add(16);
+
+			Fruit_D.attr = new List<int>();
+			Fruit_D.attr.Add(7);
+			Fruit_D.attr.Add(0);
+			Fruit_D.attr.Add(-2);
+
+			Fruit_D.level = new List<int>();
+			Fruit_D.level.Add(3);
+			Fruit_D.level.Add(4);
+			Fruit_D.level.Add(5);
+
+			map.Add(3, Fruit_D);
+
+			// =================================
+			// E #4, Fruit_E
+			FOOD Fruit_E;
+
+			Fruit_E.name = FoodType.Cucumber;
+
+			Fruit_E.shapes = new List<int>();
+			Fruit_E.shapes.Add(6);
+			Fruit_E.shapes.Add(7);
+			Fruit_E.shapes.Add(14);
+			Fruit_E.shapes.Add(16);
+
+			Fruit_E.attr = new List<int>();
+			Fruit_E.attr.Add(4);
+			Fruit_E.attr.Add(0);
+			Fruit_E.attr.Add(0);
+
+			Fruit_E.level = new List<int>();
+			Fruit_E.level.Add(4);
+			Fruit_E.level.Add(5);
+
+			map.Add(4, Fruit_E);
+
+			// =================================
+			// M_1 #5, Meat_M_1
+			FOOD Meat_M_1;
+
+			Meat_M_1.name = FoodType.Steak;
+
+			Meat_M_1.shapes = new List<int>();
+			Meat_M_1.shapes.Add(2);
+			Meat_M_1.shapes.Add(4);
+			Meat_M_1.shapes.Add(5);
+
+			Meat_M_1.attr = new List<int>();
+			Meat_M_1.attr.Add(0);
+			Meat_M_1.attr.Add(2);
+			Meat_M_1.attr.Add(0);
+
+			Meat_M_1.level = new List<int>();
+			Meat_M_1.level.Add(1);
+			Meat_M_1.level.Add(2);
+			Meat_M_1.level.Add(3);
+
+			map.Add(5, Meat_M_1);
+
+			// =================================
+			// M_2 #6, Meat_M_2
+			FOOD Meat_M_2;
+
+			Meat_M_2.name = FoodType.Drumstick;
+
+			Meat_M_2.shapes = new List<int>();
+			Meat_M_2.shapes.Add(2);
+			Meat_M_2.shapes.Add(3);
+			Meat_M_2.shapes.Add(6);
+			Meat_M_2.shapes.Add(8);
+
+			Meat_M_2.attr = new List<int>();
+			Meat_M_2.attr.Add(1);
+			Meat_M_2.attr.Add(3);
+			Meat_M_2.attr.Add(0);
+
+			Meat_M_2.level = new List<int>();
+			Meat_M_2.level.Add(2);
+			Meat_M_2.level.Add(3);
+			Meat_M_2.level.Add(4);
+
+			map.Add(6, Meat_M_2);
+
+			// =================================
+			// M_3 #7, Meat_M_3
+			FOOD Meat_M_3;
+
+			Meat_M_3.name = FoodType.Fish;
+
+			Meat_M_3.shapes = new List<int>();
+			Meat_M_3.shapes.Add(2);
+			Meat_M_3.shapes.Add(9);
+			Meat_M_3.shapes.Add(10);
+			Meat_M_3.shapes.Add(12);
+			Meat_M_3.shapes.Add(14);
+			Meat_M_3.shapes.Add(15);
+
+			Meat_M_3.attr = new List<int>();
+			Meat_M_3.attr.Add(0);
+			Meat_M_3.attr.Add(4);
+			Meat_M_3.attr.Add(-1);
+
+			Meat_M_3.level = new List<int>();
+			Meat_M_3.level.Add(1);
+			Meat_M_3.level.Add(2);
+			Meat_M_3.level.Add(3);
+			Meat_M_3.level.Add(4);
+			Meat_M_3.level.Add(5);
+
+			map.Add(7, Meat_M_3);
+
+			// =================================
+			// M_4 #8, Meat_M_4
+			FOOD Meat_M_4;
+
+			Meat_M_4.name = FoodType.BeefHead;
+
+			Meat_M_4.shapes = new List<int>();
+			Meat_M_4.shapes.Add(7);
+			Meat_M_4.shapes.Add(11);
+			Meat_M_4.shapes.Add(12);
+			Meat_M_4.shapes.Add(15);
+
+			Meat_M_4.attr = new List<int>();
+			Meat_M_4.attr.Add(-1);
+			Meat_M_4.attr.Add(6);
+			Meat_M_4.attr.Add(-2);
+
+			Meat_M_4.level = new List<int>();
+			Meat_M_4.level.Add(3);
+			Meat_M_4.level.Add(4);
+			Meat_M_4.level.Add(5);
+
+			map.Add(8, Meat_M_4);
+
+			// =================================
+			// M_5 #9, Meat_M_5
+			FOOD Meat_M_5;
+
+			Meat_M_5.name = FoodType.RoastedWholeLamb;
+
+			Meat_M_5.shapes = new List<int>();
+			Meat_M_5.shapes.Add(12);
+			Meat_M_5.shapes.Add(15);
+			Meat_M_5.shapes.Add(16);
+
+			Meat_M_5.attr = new List<int>();
+			Meat_M_5.attr.Add(-1);
+			Meat_M_5.attr.Add(5);
+			Meat_M_5.attr.Add(0);
+
+			Meat_M_5.level = new List<int>();
+			Meat_M_5.level.Add(4);
+			Meat_M_5.level.Add(5);
+
+			map.Add(9, Meat_M_5);
+
+			// =================================
+			// F_1 #10, Flower_F_1
+			FOOD Flower_F_1;
+
+			Flower_F_1.name = FoodType.WhiteCarnation;
+
+			Flower_F_1.shapes = new List<int>();
+			Flower_F_1.shapes.Add(2);
+			Flower_F_1.shapes.Add(8);
+			Flower_F_1.shapes.Add(10);
+			Flower_F_1.shapes.Add(16);
+
+			Flower_F_1.attr = new List<int>();
+			Flower_F_1.attr.Add(0);
+			Flower_F_1.attr.Add(0);
+			Flower_F_1.attr.Add(1);
+
+			Flower_F_1.level = new List<int>();
+			Flower_F_1.level.Add(1);
+			Flower_F_1.level.Add(3);
+
+			map.Add(10, Flower_F_1);
+
+			// =================================
+			// F_2 #11, Flower_F_2
+			FOOD Flower_F_2;
+
+			Flower_F_2.name = FoodType.YellowChrysanthemum;
+
+			Flower_F_2.shapes = new List<int>();
+			Flower_F_2.shapes.Add(1);
+			Flower_F_2.shapes.Add(12);
+			Flower_F_2.shapes.Add(15);
+
+			Flower_F_2.attr = new List<int>();
+			Flower_F_2.attr.Add(-1);
+			Flower_F_2.attr.Add(0);
+			Flower_F_2.attr.Add(2);
+
+			Flower_F_2.level = new List<int>();
+			Flower_F_2.level.Add(2);
+			Flower_F_2.level.Add(4);
+
+			map.Add(11, Flower_F_2);
+
+			// =================================
+			// F_3 #12, Flower_F_3
+			FOOD Flower_F_3;
+
+			Flower_F_3.name = FoodType.RedRose;
+
+			Flower_F_3.shapes = new List<int>();
+			Flower_F_3.shapes.Add(3);
+			Flower_F_3.shapes.Add(6);
+			Flower_F_3.shapes.Add(7);
+
+			Flower_F_3.attr = new List<int>();
+			Flower_F_3.attr.Add(0);
+			Flower_F_3.attr.Add(-1);
+			Flower_F_3.attr.Add(3);
+
+			Flower_F_3.level = new List<int>();
+			Flower_F_3.level.Add(1);
+			Flower_F_3.level.Add(2);
+			Flower_F_3.level.Add(3);
+			Flower_F_3.level.Add(4);
+
+			map.Add(12, Flower_F_3);
+		}
+
+		public Dictionary<int, FOOD> getFoodList()
+        {
+			return map;
+        }
+
+
+		public int[] GenerateUniqueRandom(int minValue, int maxValue, int n)
+		{
+			if (n > maxValue - minValue + 1)
+				n = maxValue - minValue + 1;
+
+			int maxIndex = maxValue - minValue + 2;// 索引数组上限
+			int[] indexArr = new int[maxIndex];
+			for (int i = 0; i < maxIndex; i++)
+			{
+				indexArr[i] = minValue - 1;
+				minValue++;
+			}
+
+			Random ran = new Random();
+			int[] randNum = new int[n];
+			int index;
+			for (int j = 0; j < n; j++)
+			{
+				index = ran.Next(1, maxIndex - 1);// 生成一个随机数作为索引
+				randNum[j] = indexArr[index];
+				indexArr[index] = indexArr[maxIndex - 1];
+				maxIndex--; //索引上限减 1
+			}
+			return randNum;
+		}
+
+		//全局随机生产食物
+		public int[] ProduceFood(uint level)
+		{
+			int numOfFood = 0;
+			//根据关卡数生产食物
+			if(level == 1)
+            {
+				numOfFood = 3;
+			}
+			else if (level == 2)
+			{
+				numOfFood = 4;
+			}
+			else if (level == 3)
+			{
+				numOfFood = 5;
+			}
+			else if (level == 4)
+			{
+				numOfFood = 6;
+			}
+			else if (level == 5)
+			{
+				numOfFood = 7;
+			}
+            else
+            {
+				numOfFood = 0;
+			}
+
+			int[] foods;
+			bool qualifiedFlag = true;
+			//循环
+			do
+			{
+				foods = GenerateUniqueRandom(0, 12, numOfFood);
+
+				foreach (int food in foods)
+				{
+					FOOD Food = map[food];
+					if (!Food.level.Contains(numOfFood))
+					{
+						qualifiedFlag = false;
+						break;
+					}
+
+					qualifiedFlag = true;
+				}
+
+			} while (!qualifiedFlag);
+
+			return foods;
+		}
+	}
+
+	class execute
+	{
+		static void Main(string[] args)
+		{
+			CFood foodList = new CFood();
+			foodList.init();
+			Dictionary<int, FOOD> mapList = foodList.getFoodList();
+		}
+	}
+
+}
---
+++ Assets/FoodManager/Rotation.cs
@@ -0,0 +1,494 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class CRotation : ScriptableObject
+{
+    public static Dir GetNextDir(Dir dir)
+    {
+        //顺时针
+        switch (dir)
+        {
+            default:
+            case Dir.Down: return Dir.Left;
+            case Dir.Left: return Dir.Up;
+            case Dir.Up: return Dir.Right;
+            case Dir.Right: return Dir.Down;
+        }
+    }
+
+    //朝向
+    public static Vector2Int GetDirForwardVector(Dir dir)
+    {
+        switch (dir)
+        {
+            default:
+            case Dir.Down: return new Vector2Int(0, -1);
+            case Dir.Left: return new Vector2Int(-1, 0);
+            case Dir.Up: return new Vector2Int(0, +1);
+            case Dir.Right: return new Vector2Int(+1, 0);
+        }
+    }
+
+    //根据位置变化确定旋转方向
+    public static Dir GetDir(Vector2Int from, Vector2Int to)
+    {
+        if (from.x < to.x)
+        {
+            return Dir.Right;
+        }
+        else
+        {
+            if (from.x > to.x)
+            {
+                return Dir.Left;
+            }
+            else
+            {
+                if (from.y < to.y)
+                {
+                    return Dir.Up;
+                }
+                else
+                {
+                    return Dir.Down;
+                }
+            }
+        }
+    }
+
+    public enum Dir
+    {
+        Down,
+        Left,
+        Up,
+        Right,
+    }
+
+    //获取旋转角度
+    public int GetRotationAngle(Dir dir)
+    {
+        switch (dir)
+        {
+            default:
+            case Dir.Down: return 0;
+            case Dir.Left: return 90;
+            case Dir.Up: return 180;
+            case Dir.Right: return 270;
+        }
+    }
+
+    public uint[,] GetFoodNewShape(uint vShape, Dir dir)
+    {
+
+        if(vShape == 1)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:                                       
+                case Dir.Up:
+                case Dir.Left:
+                case Dir.Right:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 0, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat;
+            }
+        }
+        else if (vShape == 2)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                case Dir.Up:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 0, 0, 0},
+                                         { 1, 1, 0, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                case Dir.Right:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat1;
+            }
+        }
+        else if (vShape == 3)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                case Dir.Up:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 1, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                case Dir.Right:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat1;
+            }
+        }
+        else if (vShape == 4)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 1, 1, 0, 0}};
+                    return shapeMat;
+                case Dir.Up:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat1;
+
+                case Dir.Left:
+                    uint[,] shapeMat2 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat2;
+                case Dir.Right:
+                    uint[,] shapeMat3 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 1, 0, 0},
+                                         { 1, 1, 0, 0}};
+                    return shapeMat3;
+            }
+        }
+        else if (vShape == 5)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                    uint[,] shapeMat3 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 1, 0, 0},
+                                         { 1, 1, 0, 0}};
+                    return shapeMat3;
+                case Dir.Left:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 1, 1, 0, 0}};
+                    return shapeMat;
+                case Dir.Up:
+                    uint[,] shapeMat2 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat2;
+                case Dir.Right:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat1;
+            }
+        }
+        else if (vShape == 6)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 0, 0, 1, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 1, 0, 0 },
+                                         { 0, 1, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat1;
+                case Dir.Up:
+                    uint[,] shapeMat2 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 0, 1, 1, 0}};
+                    return shapeMat2;
+                case Dir.Right:
+                    uint[,] shapeMat3 = { { 0, 0, 0, 0},
+                                         { 0, 1, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat3;
+            }
+        }
+        else if (vShape == 7)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 1, 0, 0, 0 },
+                                         { 0, 1, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 0, 1, 0},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat1;
+                case Dir.Up:
+                    uint[,] shapeMat2 = { { 0, 0, 0, 0},
+                                         { 0, 1, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat2;
+                case Dir.Right:
+                    uint[,] shapeMat3 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 1, 0, 0},
+                                         { 1, 0, 1, 0}};
+                    return shapeMat3;
+            }
+        }
+        else if (vShape == 8)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                case Dir.Up:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 1, 0, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                case Dir.Right:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 0, 0, 0},
+                                         { 1, 1, 1, 0}};
+                    return shapeMat1;
+            }
+        }
+        else if (vShape == 9)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 1, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 1, 1, 0},
+                                         { 1, 1, 0, 0}};
+                    return shapeMat1;
+                case Dir.Up:
+                    uint[,] shapeMat2 = { { 0, 0, 0, 0},
+                                         { 1, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat2;
+                case Dir.Right:
+                    uint[,] shapeMat3 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 1, 1, 0},
+                                         { 1, 1, 0, 0}};
+                    return shapeMat3;
+            }
+        }
+
+        else if (vShape == 10)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                case Dir.Up:
+                    uint[,] shapeMat = { { 1, 0, 0, 0},
+                                         { 1, 0, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                case Dir.Right:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 0, 0, 0},
+                                         { 1, 1, 1, 1}};
+                    return shapeMat1;
+            }
+        }
+        else if (vShape == 11)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 1, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 1, 1, 0},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat1;
+                case Dir.Up:
+                    uint[,] shapeMat2 = { { 0, 0, 0, 0},
+                                         { 0, 1, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat2;
+                case Dir.Right:
+                    uint[,] shapeMat3 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 1, 0, 0},
+                                         { 1, 1, 1, 0}};
+                    return shapeMat3;
+            }
+        }
+        else if (vShape == 12)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                case Dir.Left:
+                case Dir.Up:
+                case Dir.Right:
+                    uint[,] shapeMat3 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 1, 1, 0, 0}};
+                    return shapeMat3;
+            }
+        }
+        else if (vShape == 13)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                case Dir.Up:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 0, 1, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                case Dir.Right:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 0, 1, 1, 0}};
+                    return shapeMat1;
+            }
+        }
+        else if (vShape == 14)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 1, 1, 0},
+                                         { 0, 0, 1, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 1, 0, 0 },
+                                         { 0, 1, 0, 0},
+                                         { 1, 1, 0, 0}};
+                    return shapeMat1;
+                case Dir.Up:
+                    uint[,] shapeMat2 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 1, 1, 1, 0}};
+                    return shapeMat2;
+                case Dir.Right:
+                    uint[,] shapeMat3 = { { 0, 0, 0, 0},
+                                         { 1, 1, 0, 0 },
+                                         { 1, 0, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat3;
+            }
+        }
+        else if (vShape == 15)
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 1, 1, 1, 1},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                    uint[,] shapeMat1 = { { 0, 1, 0, 0},
+                                         { 1, 1, 0, 0 },
+                                         { 0, 1, 0, 0},
+                                         { 0, 1, 0, 0}};
+                    return shapeMat1;
+                case Dir.Up:
+                    uint[,] shapeMat2 = { { 0, 0, 0, 0},
+                                         { 0, 0, 0, 0 },
+                                         { 0, 0, 1, 0},
+                                         { 1, 1, 1, 1}};
+                    return shapeMat2;
+                case Dir.Right:
+                    uint[,] shapeMat3 = { { 1, 0, 0, 0},
+                                         { 1, 0, 0, 0 },
+                                         { 1, 1, 0, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat3;
+            }
+        }
+        else
+        {
+            switch (dir)
+            {
+                default:
+                case Dir.Down:
+                case Dir.Up:
+                    uint[,] shapeMat = { { 0, 0, 0, 0},
+                                         { 1, 1, 0, 0 },
+                                         { 0, 1, 0, 0},
+                                         { 0, 1, 1, 0}};
+                    return shapeMat;
+                case Dir.Left:
+                case Dir.Right:
+                    uint[,] shapeMat1 = { { 0, 0, 0, 0},
+                                         { 0, 0, 1, 0 },
+                                         { 1, 1, 1, 0},
+                                         { 1, 0, 0, 0}};
+                    return shapeMat1;
+            }
+        }
+    }
+}
